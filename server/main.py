6d156256e6e6aa629af2d6ee4f60e6558bb6192e
Nguyễn Ngọc Thiện <thien.npc369@gmail.com>
2026-02-06 18:48:46 +0700

    fix: cors

--- server/main.py
+++ server/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://human-detect.ntdev.id.vn/",
         "https://human-detect.ntdev.id.vn",
     ],
     allow_credentials=True,
